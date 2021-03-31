8111ab0c86fe23ae240d53d07a937ca0df6b37fc
Sammy Krosoft <sammy@hotmail.fr>
2021-03-31 17:46:24 -0400

    special caractrers instead of space broke script

--- CollectWadeSmithExchangeM365InfoV2.ps1
+++ CollectWadeSmithExchangeM365InfoV2.ps1
@@ -83,29 +83,29 @@ If ($IncludeUserSpecificInfo){
 # In Exchange On-premises<Connect to Exchange management Shell>
 # -------------------------------------------------------------------------------------------------
 If ($OnPremExchangeManagementShellCommands){
-    Get-FederationTrust | Set-FederationTrust -RefreshMetadata 
-    Get-AutoDiscoverVirtualDirectory | FL > $OutputFilesCollection[6]
-    Get-AvailabilityAddressSpace | FL > $OutputFilesCollection[3]
-    Get-FederatedOrganizationIdentifier | FL > $OutputFilesCollection[8]
-    Get-FederationTrust | FL > $OutputFilesCollection[7]
-    Get-HybridConfiguration | FL > $OutputFilesCollection[15]
-    Get-IntraOrganizationConnector | FL > $OutputFilesCollection[2]
-    Get-OrganizationRelationship | FL > $OutputFilesCollection[0]
-    Get-ExchangeServer | FT name, serverrole, AdminDisplayVersion > $OutputFilesCollection[14]
-    Get-SharingPolicy | FL > $OutputFilesCollection[4]
-    Test-FederationTrustCertificate | FL > $OutputFilesCollection[11]
-    Get-WebServicesVirtualDirectory | FL > $OutputFilesCollection[5]
+    Get-FederationTrust | Set-FederationTrust -RefreshMetadata 
+    Get-AutoDiscoverVirtualDirectory | FL > $OutputFilesCollection[6]
+    Get-AvailabilityAddressSpace | FL > $OutputFilesCollection[3]
+    Get-FederatedOrganizationIdentifier | FL > $OutputFilesCollection[8]
+    Get-FederationTrust | FL > $OutputFilesCollection[7]
+    Get-HybridConfiguration | FL > $OutputFilesCollection[15]
+    Get-IntraOrganizationConnector | FL > $OutputFilesCollection[2]
+    Get-OrganizationRelationship | FL > $OutputFilesCollection[0]
+    Get-ExchangeServer | FT name, serverrole, AdminDisplayVersion > $OutputFilesCollection[14]
+    Get-SharingPolicy | FL > $OutputFilesCollection[4]
+    Test-FederationTrustCertificate | FL > $OutputFilesCollection[11]
+    Get-WebServicesVirtualDirectory | FL > $OutputFilesCollection[5]
 
     If ($IncludeUserSpecificInfo){
         # User specific informtion
-        Get-FederationInformation -Domainname $CustomerOnMicrosoftDomain | FL > $OutputFilesCollection[9]
+        Get-FederationInformation -Domainname $CustomerOnMicrosoftDomain | FL > $OutputFilesCollection[9]
         # User specific information
-        Get-Mailbox $OnPremisesMailbox | FL > $OutputFilesCollection[13]
+        Get-Mailbox $OnPremisesMailbox | FL > $OutputFilesCollection[13]
         # User specific information
-        Get-RemoteMailbox $CloudMailbox | FL > $OutputFilesCollection[12]
+        Get-RemoteMailbox $CloudMailbox | FL > $OutputFilesCollection[12]
         # User specific information
-        Test-FederationTrust -USerIdentity $OnPremisesMailbox > $OutputFilesCollection[10]
-        Test-OrganizationRelationship -Identity "On-premises to O365 Organization Relationship" -UserIdentity $OnPremisesMailbox -Verbose > $OutputFilesCollection[1]
+        Test-FederationTrust -USerIdentity $OnPremisesMailbox > $OutputFilesCollection[10]
+        Test-OrganizationRelationship -Identity "On-premises to O365 Organization Relationship" -UserIdentity $OnPremisesMailbox -Verbose > $OutputFilesCollection[1]
     }
 }
 
@@ -113,20 +113,20 @@ If ($OnPremExchangeManagementShellCommands){
 # In Exchange Online<Connect to Exchange Online service>：  
 # -------------------------------------------------------------------------------------------------
 If ($OnLineExchangeManagementShellCommands){
-    Get-AvailabilityAddressSpace |  FL > $OutputFilesCollection[19]
-    Get-FederatedOrganizationIdentifier | FL > $OutputFilesCollection[23]
-    Get-FederationTrust | FL > $OutputFilesCollection[21]
-    Get-IntraOrganizationConnector | FL > $OutputFilesCollection[18]
-    Get-OrganizationRelationship | FL > $OutputFilesCollection[16]
-    Get-SharingPolicy | FL > $OutputFilesCollection[20]
+    Get-AvailabilityAddressSpace |  FL > $OutputFilesCollection[19]
+    Get-FederatedOrganizationIdentifier | FL > $OutputFilesCollection[23]
+    Get-FederationTrust | FL > $OutputFilesCollection[21]
+    Get-IntraOrganizationConnector | FL > $OutputFilesCollection[18]
+    Get-OrganizationRelationship | FL > $OutputFilesCollection[16]
+    Get-SharingPolicy | FL > $OutputFilesCollection[20]
     If ($IncludeUserSpecificInfo){
         # User specific information
-        Get-FederationInformation -DomainName $CustomerDomain | FL > $OutputFilesCollection[22]
+        Get-FederationInformation -DomainName $CustomerDomain | FL > $OutputFilesCollection[22]
         # User specific information
-        Get-Mailbox $CloudMailbox | FL > $OutputFilesCollection[25]
-        Get-MailUser $OnPremisesMailbox | FL  > $OutputFilesCollection[24]
+        Get-Mailbox $CloudMailbox | FL > $OutputFilesCollection[25]
+        Get-MailUser $OnPremisesMailbox | FL  > $OutputFilesCollection[24]
         # User specific information
-        Test-OrganizationRelationship -UserIdentity $CloudMailbox  -Identity "Exchange Online to On Premises Organization Relationship" -Verbose > $OutputFilesCollection[17]
+        Test-OrganizationRelationship -UserIdentity $CloudMailbox  -Identity "Exchange Online to On Premises Organization Relationship" -Verbose > $OutputFilesCollection[17]
     }
 }
 
@@ -135,17 +135,17 @@ If ($OnLineExchangeManagementShellCommands){
 # In Exchange On-premises<Connect to Exchange management Shell>： 
 # -------------------------------------------------------------------------------------------------
 If ($OnPremExchangeManagementShellCommands){
-    Get-AuthConfig | FL > $OutputFilesCollection[31]
-    Get-ExchangeCertificate -Thumbprint (Get-AuthConfig).CurrentCertificateThumbprint | FL > $OutputFilesCollection[32]
-    Get-AuthServer | FL > $OutputFilesCollection[28]
-    Foreach ($i in (Get-ExchangeServer)) {Write-Host $i.FQDN; Get-ExchangeCertificate -Server $i.Identity} > $OutputFilesCollection[42]
-    Get-IntraOrganizationConfiguration | FL > $OutputFilesCollection[27]
-    Get-PartnerApplication | FL > $OutputFilesCollection[29]
-    Get-PartnerApplication 00000002-0000-0ff1-ce00-000000000000 | Select-Object -ExpandProperty LinkedAccount | Get-User | FL > $OutputFilesCollection[30]
+    Get-AuthConfig | FL > $OutputFilesCollection[31]
+    Get-ExchangeCertificate -Thumbprint (Get-AuthConfig).CurrentCertificateThumbprint | FL > $OutputFilesCollection[32]
+    Get-AuthServer | FL > $OutputFilesCollection[28]
+    Foreach ($i in (Get-ExchangeServer)) {Write-Host $i.FQDN; Get-ExchangeCertificate -Server $i.Identity} > $OutputFilesCollection[42]
+    Get-IntraOrganizationConfiguration | FL > $OutputFilesCollection[27]
+    Get-PartnerApplication | FL > $OutputFilesCollection[29]
+    Get-PartnerApplication 00000002-0000-0ff1-ce00-000000000000 | Select-Object -ExpandProperty LinkedAccount | Get-User | FL > $OutputFilesCollection[30]
     If ($IncludeUserSpecificInfo){
         # User specific information
-        Test-OAuthConnectivity -Service AutoD  -TargetUri https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc -Mailbox $OnPremisesMailbox -Verbose | FL > $OutputFilesCollection[38]
-        Test-OAuthConnectivity -Service EWS -TargetUri https://outlook.office365.com/ews/exchange.asmx -Mailbox $OnPremisesMailbox -Verbose | FL > $OutputFilesCollection[37]
+        Test-OAuthConnectivity -Service AutoD  -TargetUri https://autodiscover-s.outlook.com/autodiscover/autodiscover.svc -Mailbox $OnPremisesMailbox -Verbose | FL > $OutputFilesCollection[38]
+        Test-OAuthConnectivity -Service EWS -TargetUri https://outlook.office365.com/ews/exchange.asmx -Mailbox $OnPremisesMailbox -Verbose | FL > $OutputFilesCollection[37]
     }
 }
 
@@ -155,18 +155,18 @@ If ($OnPremExchangeManagementShellCommands){
 If ($OnLineExchangeManagementShellCommands){
     if ($IncludeUserSpecificInfo){
         # User specific information
-        Test-OAuthConnectivity -Service AutoD -TargetUri $OnPremisesAutodiscoverURL -Mailbox $CloudMailbox -Verbose | FL > $OutputFilesCollection[49]
-        Test-OAuthConnectivity -Service EWS -TargetUri $OnPremisesExternalEWSURL -Mailbox $CloudMailbox -Verbose | FL > $OutputFilesCollection[48]
+        Test-OAuthConnectivity -Service AutoD -TargetUri $OnPremisesAutodiscoverURL -Mailbox $CloudMailbox -Verbose | FL > $OutputFilesCollection[49]
+        Test-OAuthConnectivity -Service EWS -TargetUri $OnPremisesExternalEWSURL -Mailbox $CloudMailbox -Verbose | FL > $OutputFilesCollection[48]
     }
-    Get-AuthServer -Identity 00000001-0000-0000-c000-000000000000 | FL > $OutputFilesCollection[46]
-    Get-IntraOrganizationConfiguration | FL > $OutputFilesCollection[45]
-    Get-PartnerApplication | FL > $OutputFilesCollection[47]
+    Get-AuthServer -Identity 00000001-0000-0000-c000-000000000000 | FL > $OutputFilesCollection[46]
+    Get-IntraOrganizationConfiguration | FL > $OutputFilesCollection[45]
+    Get-PartnerApplication | FL > $OutputFilesCollection[47]
 }
 # -------------------------------------------------------------------------------------------------
 # Azure/MSOLPowershell: 
 # -------------------------------------------------------------------------------------------------
 If ($MSOLCommands){
-    Get-MsolServicePrincipal -ServicePrincipalName "00000002-0000-0ff1-ce00-000000000000" | FL  > $OutputFilesCollection[53]
-    (Get-MsolServicePrincipal -ServicePrincipalName "00000002-0000-0ff1-ce00-000000000000").ServicePrincipalNames > $OutputFilesCollection[54]
-    Get-MsolServicePrincipalCredential -ServicePrincipalName "00000002-0000-0ff1-ce00-000000000000" -ReturnKeyValues $true > $OutputFilesCollection[55]
+    Get-MsolServicePrincipal -ServicePrincipalName "00000002-0000-0ff1-ce00-000000000000" | FL  > $OutputFilesCollection[53]
+    (Get-MsolServicePrincipal -ServicePrincipalName "00000002-0000-0ff1-ce00-000000000000").ServicePrincipalNames > $OutputFilesCollection[54]
+    Get-MsolServicePrincipalCredential -ServicePrincipalName "00000002-0000-0ff1-ce00-000000000000" -ReturnKeyValues $true > $OutputFilesCollection[55]
 }
